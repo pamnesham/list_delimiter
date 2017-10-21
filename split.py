26bf59bbf5f5babf934d163052f9c49565c925b6
pamnesham <nesha002@umn.edu>
2017-10-21 01:23:04 -0500

    comment edited

--- split.py
+++ split.py
@@ -3,8 +3,6 @@
 #This function breaks a list into sublists based on specified delimiter
 def mysplit(stringarg, delimiter):
     subsList = []
-    '''idx = stringarg.find(delimiter)
-    print(idx)'''
     while len(stringarg) > 0:
         idx = stringarg.find(delimiter)
         if idx < 0:
